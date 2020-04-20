7b8b334ae9f6d80bb198dee77e8e356a00aeb143
Ryan Moore <Smacherman@gmail.com>
2020-04-19 21:43:43 -0700

    BT63 - Adding a Quit button

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -19,10 +19,6 @@ class BATTLETANK_API ATankAIController : public AAIController
     virtual void BeginPlay() override;
     
     virtual void Tick(float DeltaSeconds) override;
-    
-    ATank* GetControlledTank() const;
-    
-    ATank* GetPlayerTank() const;
 
 };
 
