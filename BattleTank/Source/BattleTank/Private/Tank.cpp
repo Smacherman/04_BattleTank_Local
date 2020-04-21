6b257762a4a4cb8f139faadbe1c28fa8ffa08936
Ryan Moore <Smacherman@gmail.com>
2020-04-20 18:29:24 -0700

    BT67 - Using BlueprintReadOnly

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -4,6 +4,7 @@
 #include "TankBarrel.h"
 #include "Projectile.h"
 #include "TankAimingComponent.h"
+#include "TankMovementComponent.h"
 #include "BattleTank/BattleTank.h"
 
 
@@ -16,6 +17,7 @@ ATank::ATank()
     
     // No need to protect points as added at construction
     TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
+    TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
 }
 
 void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
